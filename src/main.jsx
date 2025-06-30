12f6c166942c0c78dcee40430b19ff9362df5752
abedinalways <nion.sk5067@gmail.com>
2025-06-30 13:06:56 +0600

    AOS added in the project

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import router from './Routes/Router'
-
+import 'aos/dist/aos.css';
+import Aos from 'aos'
+Aos.init();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
